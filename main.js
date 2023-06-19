8dc63c5fcb6401d5226e5223cd87e1389074a5cb
jacopo.congia <jacopo.congia@gmail.com>
2023-06-19 10:29:28 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,82 @@
+import "./style.css";
+
+const searchFormEl = document.getElementById("search-bar");
+const searchBarEl = document.getElementById("search");
+const movieListEl = document.getElementById("content");
+const startExploringEl = document.getElementById("start-exploring");
+
+const movieArray = [];
+
+searchFormEl.addEventListener("submit", handleFormSubmit);
+
+document.addEventListener("click", (e) => {
+  if (e.target.dataset.watchlist) {
+    addedToWatchlist(e);
+    fetch(
+      `http://www.omdbapi.com/?apikey=50a0c6e&i=${e.target.dataset.watchlist}`
+    )
+      .then((res) => res.json())
+      .then((data) => movieArray.push(data))
+      .then(() => {
+        localStorage.setItem("watchlist", JSON.stringify(movieArray));
+      });
+  }
+});
+
+function addedToWatchlist(e) {
+  e.target.parentNode.children[2].textContent = "Added!";
+  e.target.parentNode.children[2].style = "color: #00be00";
+}
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+  if (searchBarEl.value) {
+    startExploringEl.classList.add("hide-content");
+    movieListEl.innerHTML = ``;
+    retrieveMovie(searchBarEl.value);
+    searchBarEl.value = "";
+  }
+}
+
+function retrieveMovie(title) {
+  const updatedTitle = title.replace(" ", "+");
+
+  fetch(`http://www.omdbapi.com/?apikey=50a0c6e&s=${updatedTitle}`)
+    .then((res) => res.json())
+    .then((data) => {
+      data.Search.map((movie) => {
+        fetch(
+          `http://www.omdbapi.com/?apikey=50a0c6e&t=${movie.Title.replace(
+            /[^A-Z0-9]+/gi,
+            "+"
+          )}`
+        )
+          .then((res) => res.json())
+          .then((data) => setMovieListHtml(data));
+      });
+    });
+}
+
+function setMovieListHtml(movie) {
+  const { Title, Runtime, Genre, Plot, Poster, imdbRating, imdbID } = movie;
+  movieListEl.innerHTML += `
+                <div class="movie-result-container">
+                  <img class="movie-result-poster" src=${Poster} onerror="this.src='./no-poster.png'" "alt="the movie poster"/>
+                  <div class="movie-result-info-container">
+                    <div class="movie-result-header">
+                      <h4 class="movie-result-title">${Title}</h4>
+                      <img class="movie-result-star" src="./star.png" />
+                      <p class="movie-result-rating">${imdbRating}</p>
+                    </div>
+                    <div class="movie-result-details">
+                      <p class="movie-result-length">${Runtime}</p>
+                      <p class="movie-result-genre">${Genre}</p>
+                      <div class="movie-result-watchlist-add" data-watchlist=${imdbID}>
+                        <img class="movie-result-watchlist-add-icon" src="./add.png" />
+                        <p>Watchlist</p>
+                      </div>
+                    </div>
+                    <p class="movie-result-synopsis">${Plot}</p>
+                  </div>
+                </div>`;
+}
---
+++ watchlist.js
@@ -0,0 +1,63 @@
+const movieListEl = document.getElementById("content");
+const nothingHereYetEl = document.getElementById("nothing-here-yet");
+let movieArray = getLocalStorage();
+
+function getLocalStorage() {
+  return JSON.parse(localStorage.getItem("watchlist"));
+}
+
+function setLocalStorage() {
+  return localStorage.setItem("watchlist", JSON.stringify(movieArray));
+}
+
+document.addEventListener("click", (e) => {
+  if (e.target.dataset.watchlist) {
+    movieArray = movieArray.filter((movie) => {
+      if (movie.imdbID !== e.target.dataset.watchlist) {
+        return movie;
+      }
+    });
+
+    getLocalStorage();
+    render();
+    setLocalStorage();
+  }
+});
+
+function render() {
+  movieListEl.innerHTML = ``;
+  if (movieArray.length) {
+    nothingHereYetEl.classList.add("hide-content");
+    setMovieListHtml(movieArray);
+  } else if (movieArray.length === 0) {
+    nothingHereYetEl.classList.remove("hide-content");
+  }
+}
+
+function setMovieListHtml(movieArray) {
+  movieArray.map((movie) => {
+    const { Title, Runtime, Genre, Plot, Poster, imdbRating, imdbID } = movie;
+    movieListEl.innerHTML += `
+                        <div class="movie-result-container">
+                          <img class="movie-result-poster" src=${Poster} onerror="this.src='./no-poster.png'" "alt="the movie poster"/>
+                          <div class="movie-result-info-container">
+                            <div class="movie-result-header">
+                              <h4 class="movie-result-title">${Title}</h4>
+                              <img class="movie-result-star" src="./star.png" />
+                              <p class="movie-result-rating">${imdbRating}</p>
+                            </div>
+                            <div class="movie-result-details">
+                              <p class="movie-result-length">${Runtime}</p>
+                              <p class="movie-result-genre">${Genre}</p>
+                              <div class="movie-result-watchlist-add remove" data-watchlist=${imdbID}>
+                                <img class="movie-result-watchlist-add-icon" src="./remove.png" />
+                                <p>Remove</p>
+                              </div>
+                            </div>
+                            <p class="movie-result-synopsis">${Plot}</p>
+                          </div>
+                        </div>`;
+  });
+}
+
+render();
