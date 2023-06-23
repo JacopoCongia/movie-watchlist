ed29a23b2b3b895d6c58ce578f23be1741b649d2
jacopo.congia <jacopo.congia@gmail.com>
2023-06-23 19:20:25 +0200

    Refactor

--- watchlist.js
+++ watchlist.js
@@ -1,5 +1,5 @@
 const movieListEl = document.getElementById("content");
-const nothingHereYetEl = document.getElementById("nothing-here-yet");
+const nothingHereYetEl = document.getElementById("watchlist-empty-container");
 let movieArray = getLocalStorage();
 
 function getLocalStorage() {
@@ -38,25 +38,25 @@ function setMovieListHtml(movieArray) {
   movieArray.map((movie) => {
     const { Title, Runtime, Genre, Plot, Poster, imdbRating, imdbID } = movie;
     movieListEl.innerHTML += `
-                        <div class="movie-result-container">
-                          <img class="movie-result-poster" src=${Poster} onerror="this.src='./no-poster.png'" "alt="the movie poster"/>
-                          <div class="movie-result-info-container">
-                            <div class="movie-result-header">
-                              <h4 class="movie-result-title">${Title}</h4>
-                              <img class="movie-result-star" src="./star.png" />
-                              <p class="movie-result-rating">${imdbRating}</p>
-                            </div>
-                            <div class="movie-result-details">
-                              <p class="movie-result-length">${Runtime}</p>
-                              <p class="movie-result-genre">${Genre}</p>
-                              <div class="movie-result-watchlist-add remove" data-watchlist=${imdbID}>
-                                <img class="movie-result-watchlist-add-icon" src="./remove.png" />
-                                <p>Remove</p>
+                          <div class="movie-result-container">
+                            <img class="movie-result-poster" src=${Poster} onerror="this.src='./no-poster.png'" "alt="the movie poster"/>
+                            <div class="movie-result-info-container">
+                              <div class="movie-result-header">
+                                <h4 class="movie-result-title">${Title}</h4>
+                                <img class="movie-result-star" src="./star.png" />
+                                <p class="movie-result-rating">${imdbRating}</p>
                               </div>
+                              <div class="movie-result-details">
+                                <p class="movie-result-length">${Runtime}</p>
+                                <p class="movie-result-genre">${Genre}</p>
+                                <div class="movie-result-watchlist-add remove" data-watchlist=${imdbID}>
+                                  <img class="movie-result-watchlist-add-icon" src="./remove.png" />
+                                  <p>Remove</p>
+                                </div>
+                              </div>
+                              <p class="movie-result-synopsis">${Plot}</p>
                             </div>
-                            <p class="movie-result-synopsis">${Plot}</p>
-                          </div>
-                        </div>`;
+                          </div>`;
   });
 }
 
